ec885cbdf612b1e48cdfdadc6f13b6286b40398e
Elva0707 <168251021+Elva0707@users.noreply.github.com>
2025-10-17 21:36:34 -0500

    mascotas
    crud de mascotas

---
+++ app/Models/Mascota.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mascota extends Model
+{
+     use HasFactory;
+
+    protected $table = 'mascota';
+    protected $primaryKey = 'id_mascota';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre',
+        'raza',
+        'edad',
+        'id_propietario'
+    ];
+
+    // Relación con Propietario
+    public function propietario()
+    {
+        return $this->belongsTo(Propietario::class, 'id_propietario', 'id_propietario');
+    }
+}
